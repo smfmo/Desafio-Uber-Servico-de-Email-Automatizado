23d112a590c9902a25da3cb752aa3799eb72b03a
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-13 16:01:38 -0300

    Commit initial: Create project

---
+++ src/test/java/io/github/smfmo/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.smfmo.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
