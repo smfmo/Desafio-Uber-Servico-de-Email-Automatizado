2f510a9179e1cf30cecf39720983235a53d10267
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-13 16:55:31 -0300

    Criando o serviço que implementa eo contrato de caso de uso da aplicação onde faz a ponte dos serviços externos para o core

---
+++ src/main/java/io/github/smfmo/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package io.github.smfmo.email_service.application;
+
+import io.github.smfmo.email_service.adapters.EmailSenderGateway;
+import io.github.smfmo.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+       this.emailSenderGateway = emailSenderGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
