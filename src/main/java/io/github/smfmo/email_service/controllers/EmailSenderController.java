a7b92d2fe20ad54e915d39f01de3265798448beb
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-13 17:02:40 -0300

    Implementação do controller onde será feita as requisições HTTP

---
+++ src/main/java/io/github/smfmo/email_service/controllers/EmailSenderController.java
@@ -0,0 +1,19 @@
+package io.github.smfmo.email_service.controllers;
+
+import io.github.smfmo.email_service.application.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+}
