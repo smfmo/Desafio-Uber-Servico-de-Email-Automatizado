2c2f91896212dba418db2025d9a88051dc0d6c0b
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-14 08:58:41 -0300

    Implementation of method post to trigger the email sending request

--- src/main/java/io/github/smfmo/email_service/controllers/EmailSenderController.java
+++ src/main/java/io/github/smfmo/email_service/controllers/EmailSenderController.java
@@ -1,7 +1,13 @@
 package io.github.smfmo.email_service.controllers;
 
 import io.github.smfmo.email_service.application.EmailSenderService;
+import io.github.smfmo.email_service.core.EmailRequest;
+import io.github.smfmo.email_service.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +20,20 @@ public class EmailSenderController {
     @Autowired
     public EmailSenderController(EmailSenderService emailSenderService) {
         this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try{
+            this.emailSenderService.sendEmail(
+                    request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("email sent successfully");
+
+        } catch (EmailServiceException exception) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error while sending email");
+        }
     }
 
 }
