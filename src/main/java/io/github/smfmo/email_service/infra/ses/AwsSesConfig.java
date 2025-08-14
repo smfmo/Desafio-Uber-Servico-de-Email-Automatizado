f801085f4a087d925c9ddb87b2c0d7b31dedae2f
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-14 09:02:57 -0300

    Implementation of Bean for managenment of AmazonSimpleEmailService

--- src/main/java/io/github/smfmo/email_service/infra/ses/AwsSesConfig.java
+++ src/main/java/io/github/smfmo/email_service/infra/ses/AwsSesConfig.java
@@ -1,14 +1,34 @@
 package io.github.smfmo.email_service.infra.ses;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SesEmailSenderConfiguration {
+public class AwsSesConfig {
+
+    @Value("${aws.accessKeyId}")
+    private  String awsAccessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
 
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return null;
+        AWSCredentials credentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretKey);
+
+        return AmazonSimpleEmailServiceAsyncClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(awsRegion)
+                .build();
     }
 }
